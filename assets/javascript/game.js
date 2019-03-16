9183f08ebb06e69a48a0141a1be03473b38ac217
benclark1110 <benclark1110@gmail.com>
2019-03-16 00:39:16 -0400

    HTML Skeleton + Basic js

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var movies = ["pulp fictopn", "django unchained", "the avengers", "the hateful eight", "shaft"];
+
+var randomMovie = movies[Math.floor(Math.random() * movies.length)];
+console.log(randomMovie);
+
+/*Not sure aboout below*/
+var movieSpacesElem = document.getElementById("movieSpaces");
