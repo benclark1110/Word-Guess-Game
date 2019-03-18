4064b857bca7dbd01aec815a798b65d6c61f17ef
benclark1110 <benclark1110@gmail.com>
2019-03-17 23:46:35 -0400

    able to check for letters in a word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,29 @@
-var movies = ["pulp fictopn", "django unchained", "the avengers", "the hateful eight", "shaft"];
+var movies = ["pulp fiction", "django unchained", "the avengers", "the hateful eight", "shaft"];
 
 var randomMovie = movies[Math.floor(Math.random() * movies.length)];
 console.log(randomMovie);
 
-/*Not sure aboout below*/
-var movieSpacesElem = document.getElementById("movieSpaces");
+document.onkeyup = function(event) {
+
+    //Setting original Value to False.
+    letterPresent = false;
+    missedLetters = []
+
+    for (i = 0; i < randomMovie.length; i++){
+        if (randomMovie.includes(event.key)){
+            letterPresent = true;
+            console.log(letterPresent);
+            //Will need to add code to call a function to show that letter
+        }
+        
+        else if (!letterPresent && !missedLetters.includes(event.key)) {
+            missedLetters.push(event.key);
+            console.log(missedLetters);
+        }
+    }
+
+    var lettersAlreadyGuessedInput = document.getElementById("lettersAlreadyGuessed");
+
+    lettersAlreadyGuessedInput.textContent = missedLetters;
+
+}
