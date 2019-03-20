ad33a3db722091ca779cc525092a35a6d8449582
benclark1110 <benclark1110@gmail.com>
2019-03-19 23:49:09 -0400

    Finishing touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,10 @@
-//Add Document Ready here
-
-//Set of movies
-var movies = ["pulpfiction", "djangounchained", "theavengers", "thehatefuleight", "shaft"];
-//Randomly select movie from above
-var randomMovie = movies[Math.floor(Math.random() * movies.length)];
+//Set of characters
+var characters = ["maeby", "tobias", "lucille", "gob", "michael", "lindsay", "oscar"];
+//Randomly select character from above
+var randomCharacters = characters[Math.floor(Math.random() * characters.length)];
 //Array to hold answer characters
 var answerArray = [];
-console.log(randomMovie);
+console.log(randomCharacters);
 //Array to hold incorrect guesses
 var missedLetters = [];
 //Array to hold correct Guesses
@@ -17,84 +15,94 @@ var losses = 0;
 var wins = 0;
 //Setting Remaining Guesses
 var numberOfGuessesRemaining = 6;
+//Variable for acceptable characters
+var allowedCharacters = "qwertyuiopasdfghjklzxcvbnm"
+//Theme song
+var audio = new Audio('assets/music/arrestedDevelopmentTheme.mp3');
 
-    function callMovie(){
-        for (i = 0; i < randomMovie.length; i++) {
-            answerArray[i] = "_";
-            console.log(answerArray)
-        } // can this go below?
-    }
+    document.onkeyup = function(event) {
 
-        document.onkeyup = function(event) {
-
-            //Setting original Value to False.
-            letterPresent = false;
-
-            //Checking letters from movie for a matching letter            
-            for (i = 0; i < randomMovie.length; i++) {
-                if (randomMovie[i] === event.key) {
-                    letterPresent = true;
-                    console.log(letterPresent);
-                    answerArray[i] = event.key;
-                }
-            }
-
-            //Adding to correctLetters Array if it isnt already there
-            if (letterPresent && !correctLetters.includes(event.key)) {
-                correctLetters.push(event.key);
-                console.log(correctLetters);
-            }
-
-            //Adding to missedLetters Array if it isnt already there
-            if (!letterPresent && !missedLetters.includes(event.key)) {
-                missedLetters.push(event.key);
-                console.log(missedLetters);
-                numberOfGuessesRemaining--;
-            }
-
-            //Checking for number of misses to track a loss
-            if (missedLetters.length >= 6) {
-                losses++;
-                reset();
-            }
-
-            //Checking for number of correct guesses to track a win
-            if (answerArray.includes("_") === false) {
-                wins++;
-                reset();
-                //Need to call funtion to set everything back to null and call for another random movie
-            }
-
-            //Reset Function
-            function reset (){
-                answerArray = [];
-                missedLetters = [];
-                correctLetters = [];
-                randomMovie = movies[Math.floor(Math.random() * movies.length)];
-                console.log(randomMovie)
-                letterPresent = false;
-                numberOfGuessesRemaining = 6;
-                callMovie();
-            }
-
-            var lettersAlreadyGuessedInput = document.getElementById("lettersAlreadyGuessed");
-
-            lettersAlreadyGuessedInput.textContent = missedLetters;
-
-            var movieSpacesInput = document.getElementById("movieSpaces");
-
-            movieSpacesInput.textContent = answerArray.join(" ")  
-
-            var lossesInput = document.getElementById("losses");
-
-            lossesInput.textContent = losses;
-
-            var winsInput = document.getElementById("wins");
-
-            winsInput.textContent = wins;
-
-            var numberOfGuessesRemainingInput = document.getElementById("numberOfGuessesRemaining");
-
-            numberOfGuessesRemainingInput.textContent = numberOfGuessesRemaining;
-
+        //Preventing user from selecting anything other than letters
+        if (!allowedCharacters.includes(event.key)) {
+            alert("Please use valid key")
         }
+        
+        function callCharacters(){
+            for (i = 0; i < randomCharacters.length; i++) {
+                answerArray[i] = "_";
+                console.log(answerArray)
+            }
+        }
+
+        //Setting original Value to False.
+        letterPresent = false;
+
+        //Checking letters from character for a matching letter            
+        for (i = 0; i < randomCharacters.length; i++) {
+            if (randomCharacters[i] === event.key.toLowerCase()) {
+                letterPresent = true;
+                console.log(letterPresent);
+                answerArray[i] = event.key.toLowerCase();
+            }
+        }
+
+        //Adding to correctLetters Array if it isnt already there
+        if (letterPresent && !correctLetters.includes(event.key.toLowerCase())) {
+            correctLetters.push(event.key.toLowerCase());
+            console.log(correctLetters);
+        }
+
+        //Adding to missedLetters Array if it isnt already there
+        if (!letterPresent && !missedLetters.includes(event.key.toLowerCase()) && allowedCharacters.includes(event.key)) {
+            missedLetters.push(event.key.toLowerCase());
+            console.log(missedLetters);
+            numberOfGuessesRemaining--;
+        }
+
+        //Checking for number of misses to track a loss
+        if (missedLetters.length >= 6) {
+            losses++;
+            reset();
+            audio.pause();
+        }
+
+        //Checking for number of correct guesses to track a win
+        if (answerArray.includes("_") === false) {
+            wins++;
+            reset();
+            audio.play();
+        }
+
+        //Reset Function
+        function reset (){
+            answerArray = [];
+            missedLetters = [];
+            correctLetters = [];
+            randomCharacters = characters[Math.floor(Math.random() * characters.length)];
+            console.log(randomCharacters)
+            letterPresent = false;
+            numberOfGuessesRemaining = 6;
+            callCharacters();
+        }
+
+        var lettersAlreadyGuessedInput = document.getElementById("lettersAlreadyGuessed");
+
+        lettersAlreadyGuessedInput.textContent = missedLetters;
+
+        var charactersSpacesInput = document.getElementById("charactersSpaces");
+
+        charactersSpacesInput.textContent = answerArray.join(" ")  
+
+        var lossesInput = document.getElementById("losses");
+
+        lossesInput.textContent = losses;
+
+        var winsInput = document.getElementById("wins");
+
+        winsInput.textContent = wins;
+
+        var numberOfGuessesRemainingInput = document.getElementById("numberOfGuessesRemaining");
+
+        numberOfGuessesRemainingInput.textContent = numberOfGuessesRemaining;
+
+    }
